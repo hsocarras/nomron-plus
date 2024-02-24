efd437600ae434c1ac8a377c0ff3fedb0b7d0d73
Hector E. Socarras <hsocarras1987@gmail.com>
2024-02-24 01:03:48 +0100

    some progress

--- src/hostlink.js
+++ src/hostlink.js
@@ -233,6 +233,15 @@ class HostlinkProtocol {
     */
     commandEMAreaRead(unitNo,bankNo, beginningWord = 0, numberWords = 1){
 
+        if (unitNo < 0 && unitNo > 31){
+            throw new RangeError("Error Unit Number must be between 0 and 31")
+        }
+
+        //check range for bank number
+        if (bankNo < 0 && bankNo > 0x0C){
+            throw new RangeError("Error bank Number must be between 0 and 12")
+        }
+
         //check range for beginningword
         if (beginningWord < 0 && beginningWord > 9999){
             throw new RangeError("Error at EM area read, beginning word must be between 0 and 9999")
@@ -243,7 +252,42 @@ class HostlinkProtocol {
         }
         
         let frameArray = [];
-        let commandBuffer = this.commandAreaRead(unitNo, 'RJ', beginningWord, numberWords);
+        let commandBuffer = Buffer.alloc(19);
+
+        commandBuffer[0] = beginer_char;
+
+        let strUnitNo = utils.num2str(unitNo, 2, 10);
+        //copy unit number into command buffer
+        let bufUnitNo = Buffer.from(strUnitNo, 'ascii');
+        bufUnitNo.copy(commandBuffer, 1);
+
+        //copy header into command buffer
+        let bufHeader = Buffer.from(header, 'ascii');
+        bufHeader.copy(commandBuffer, 3)
+
+        let strBankNo = utils.num2str(bankNo, 2, 16);
+        //copy unit number into command buffer
+        let bufbankNo = Buffer.from(strBankNo, 'ascii');
+        bufbankNo.copy(commandBuffer, 5);
+
+        //copy beginning word into command buffer
+        let strBeginningWord = utils.num2str(beginningWord, 4, 10);
+        let bufBeginningWord = Buffer.from(strBeginningWord, 'ascii');
+        bufBeginningWord.copy(commandBuffer, 7)
+
+        //copy number words into command buffer
+        let strNumberWords = utils.num2str(numberWords, 4, 10);
+        let bufNumberWords = Buffer.from(strNumberWords, 'ascii');
+        bufNumberWords.copy(commandBuffer, 11)
+
+        //copy fcs into command buffer
+        fcs = this.calcFCS(commandBuffer.subarray(0, 15))
+        bufFCS = Buffer.from(utils.num2str(fcs, 2, 16), 'ascii');
+        bufFCS.copy(commandBuffer, 15)
+
+        //Termination charset
+        commandBuffer[17] = terminator_char;
+        commandBuffer[18] = delimitator_char;
         
         frameArray.push(commandBuffer);
 
@@ -309,15 +353,112 @@ class HostlinkProtocol {
 
         //copy fcs into command buffer
         fcs = this.calcFCS(commandBuffer.subarray(0, 13))
-        bufFCS = Buffer.from(fcs.toString(16).toUpperCase(), 'ascii');
+        bufFCS = Buffer.from(utils.num2str(fcs, 2, 16), 'ascii');
         bufFCS.copy(commandBuffer, 13)
 
         //Termination charset
-        commandBuffer[14] = terminator_char;
-        commandBuffer[15] = delimitator_char;
+        commandBuffer[15] = terminator_char;
+        commandBuffer[16] = delimitator_char;
 
         return commandBuffer;
                
+    }
+
+    /**
+     * Utilitary function to create a response for read comands
+     * @param {number} unitNo Hostlink unit number in multilink system.
+     * @param {String} header Two char string with comand header.
+     * @param {number} endCode Two digit to inform excecution errors.
+     * @param {Buffer} readData buffer wit content for memory readed. 
+     * Bytes 0 and 1 contain word 1, byte 0 pos 0 and 1,  byte 1  pos2 and 3.
+     * @return {Array} Array of response buffer with response block
+     */
+    responseAreaRead(unitno, header, endCode, readData){
+        
+        if (unitNo < 0 && unitNo > 31){
+            throw new RangeError("Error Unit Number must be between 0 and 31")
+        }
+
+        let frameArray = [];
+        let responseBuffer;
+
+        //Every message can send up to 30 words of data.
+        if (readData.length > 60){
+
+        }
+        else{
+            let dataLength = Math.floor(readData.length/2);
+
+            responseBuffer = Buffer.alloc(11 + 4*dataLength)
+            responseBuffer[0] = beginer_char;
+
+            let strUnitNo = utils.num2str(unitNo, 2, 10);
+            //copy unit number into command buffer
+            let bufUnitNo = Buffer.from(strUnitNo, 'ascii');
+            bufUnitNo.copy(responseBuffer, 1);
+
+            //copy header into command buffer
+            let bufHeader = Buffer.from(header, 'ascii');
+            bufHeader.copy(responseBuffer, 3)
+
+            let strEndCode = utils.num2str(endCode, 2, 16);
+            //copy unit number into command buffer
+            let bufEndCode = Buffer.from(strEndCode, 'ascii');
+            bufEndCode.copy(responseBuffer, 5);
+
+            //copy words content to respose buffer in ascii format
+            let bufReadData = utils.words2AsciiBuffer(readData);
+            bufReadData.copy(responseBuffer, 7)
+
+            //copy fcs into command buffer
+            fcs = this.calcFCS(responseBuffer.subarray(0, ))
+            bufFCS = Buffer.from(utils.num2str(fcs, 2, 16), 'ascii');
+            bufFCS.copy(responseBuffer, 7 + readData.length)
+            //Termination charset
+            responseBuffer[10 + 4*dataLength] = terminator_char;
+            responseBuffer[9 + 4*dataLength] = delimitator_char;
+        }
+        
+
+    }
+
+    /**
+     * @param {Buffer} readData Buffer containing no more than 30 words
+     * @param {boolean} final Indicate it is final frame. Defaul false
+     * @returns {Buffer} response frame
+     */
+    _getSplitresponseAreaRead(readData, final = false){
+
+        let responseFrame;
+        let wordsLenght = Math.floor(readData.length / 2);
+
+        if (readData.length > 60){
+            throw new RangeError('Max frame data is 30 words');
+        }
+
+        if (final){
+            let frameLenght = 4*wordsLenght + 4;
+            responseFrame = Buffer.alloc(frameLenght);
+            responseFrame[frameLenght - 2] = terminator_char;
+            responseFrame[frameLenght - 1] = delimitator_char;
+        }
+        else{
+            let frameLenght = 4*wordsLenght + 3;
+            responseFrame = Buffer.alloc(frameLenght);
+            responseFrame[frameLenght - 1] = delimitator_char;
+        }
+
+        //copy words content to respose buffer in ascii format
+        let bufReadData = utils.words2AsciiBuffer(readData);
+        bufReadData.copy(responseBuffer);
+
+        //copy fcs into command buffer
+        fcs = this.calcFCS(responseFrame.subarray(0, 4*wordsLenght));
+        bufFCS = Buffer.from(utils.num2str(fcs, 2, 16), 'ascii');
+        bufFCS.copy(responseFrame, 4*wordsLenght);
+
+        return responseFrame;
+
     }
 
     
--- src/utils.js
+++ src/utils.js
@@ -50,6 +50,86 @@ function num2str(num, size, radix = 10) {
     return s.substring(s.length-size);
 }
 
+/**
+ * Function to get a ascii ejncoded buffer from the words buffer.
+ * @param {Buffer} words. Buffer with words value. Word is a two byte datatype. First word's byte are even buffer's byte (0, 2, 4, ...) 
+ * @param {number} radix. See Number.prototype.toString() docs
+ * @returns Buffer with word's values encoded in ascii characters.
+ */
+function words2AsciiBuffer(words, radix = 10){
+
+    let wordsCount = Math.floor(words.length / 2);
+    let buffStr = Buffer.alloc(4 * wordsCount);
+
+    for (let i = 0; i < wordsCount; i++){
+
+        //Getting hight and low bytes fron data word
+        let hsb = words[i + 1];
+        let lsb = words[i];
+    
+        //Ascii encoding
+        let strHSB = utils.num2str(hsb, 2, radix);
+        let strLSB = utils.num2str(lsb, 2, radix);
+    
+        //Creating buffer
+        let bufHSB = Buffer.from(strHSB, 'ascii');
+        let bufLSB = Buffer.from(strLSB, 'ascii');
+    
+        bufHSB.copy(buffStr, 2*i);
+        bufLSB.copy(buffStr, 1 + 2*i);
+    }
+
+    return buffStr;
+
+}
+
+/**
+ * Function to split a word's segment into multiples chunks of given size.
+ * Examle a segment of 20 words will spliten y a array of 7 chunks of 3 words if the function is called with chinkSize ecual to 3.
+ * @param {Buffer} words Buffer with words value.
+ * @param {number} chunkSize Size of chunks in number of 16 bits words.
+ * @returns {Array} Array with chunks of words.
+ */
+function splitWords(words, chunkSize = 30){
+
+    let wordsChunks = [];
+    let chunk = Buffer.alloc(2 * chunkSize);
+
+    let wordsLenght = Math.floor(words.length/2);
+
+    if(wordsLenght < chunkSize){
+        wordsChunks.push(words);
+        return wordsChunks;
+    }
+
+    let splitSize = Math.floor(wordsLenght/chunkSize);
+
+    if(wordsLenght % chunkSize > 0){
+        splitSize++
+    }
+
+    for(let i = 0; i < splitSize; i++){
+
+        let start = i*chunkSize;
+        let end = start + chunkSize;
+
+        if (end > words.length){
+            end = words.length;
+        }
+
+        chunk = words.subarray(start, end);
+        wordsChunks.push(chunk)
+    }
+
+    return wordsChunks;
+
+}
+
+
 module.exports.encodeBuff = encodeBuffer;
 
 module.exports.num2str = num2str;
+
+module.exports.words2AsciiBuffer = words2AsciiBuffer;
+
+module.exports.splitWords = splitWords;
